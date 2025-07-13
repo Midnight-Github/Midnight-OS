146d8985824c8b7cc31cb01f53d5c75145466e16
!Midnight <kevinbinoj14@gmail.com>
2025-07-13 13:18:07 +0530

    fix config values

--- os/config.lua
+++ os/config.lua
@@ -13,12 +13,12 @@ return {
   },
   settings = {
     refresh_delay = 0.5,
-    display_name = "!Midnight",
+    display_name = "Unknown",
     peripheral_module_ids = {},
     night_hour = 19,
     day_hour = 5,
     max_msg_archive_file_size = 5120,
-    hours_off_set_from_utc = 5.5,
+    hours_off_set_from_utc = 0,
     max_msg_archive_files = 10,
   },
 }
